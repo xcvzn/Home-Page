8de488387cf772206b3fa783f6edb17fc282079a
xcvzn <62747998+xcvzn@users.noreply.github.com>
2021-03-17 10:29:43 +0000

    changed search engines

--- js/search.js
+++ js/search.js
@@ -14,9 +14,9 @@ String.prototype.replaceChars = function (character, replacement) {
 
 function search(query) {
 	switch (query.substr(0, 2)) {
-		case '-d':
+		case '-g':
 			query = query.substr(3);
-			window.open('https://duckduckgo.com/' +
+			window.open('https://www.google.com/#q=' +
 				query.replaceChars(' ', '%20'), '_blank');
 			break;
 		case '-y':
@@ -27,7 +27,7 @@ function search(query) {
 		case '-w':
 			query = query.substr(3);
 			window.open(
-				'https://ru.wikipedia.org/w/index.php?search=' +
+				'https://en.wikipedia.org/w/index.php?search=' +
 				query.replaceChars(' ', '%20'), '_blank');
 			break;
 		case '-r':
@@ -36,14 +36,14 @@ function search(query) {
 				'https://www.reddit.com/search?q=' +
 				query.replaceChars(' ', '%20'), '_blank');
 			break;
-		case '-h':
-			query = query.substr(3);
-			window.open(
-				'https://wallhaven.cc/search?q=' +
-				query.concat('&categories=110&purity=100&atleast=1920x1080&sorting=relevance&order=desc'), '_blank');
-			break;
+		//case '-h':
+			//query = query.substr(3);
+			//window.open(
+				//'https://wallhaven.cc/search?q=' +
+				//query.concat('&categories=110&purity=100&atleast=1920x1080&sorting=relevance&order=desc'), '_blank');
+			//break;
 		default:
-			window.open('https://www.google.com/#q=' +
+			window.open('https://duckduckgo.com/' +
 				query.replaceChars(' ', '%20'), '_blank');
 	}
 }
@@ -57,7 +57,7 @@ searchInput.addEventListener('keyup', function (e) {
 		} else {
 			search(this.value);
 			searchInput.value = '';
-			searchInput.placeholder = '-d | -y | -w | -r | -h';
+			searchInput.placeholder = '-g | -y | -w | -r';
 		}
 	}
 });
